76b11518e6165e38b8f2e90c4cc5341033d71d26
Marcin Banik <mrn.banik@gmail.com>
2019-05-15 16:53:17 +0200

    Code separation

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+$("#hamMenu").on("click", function() {
+  jQuery(this).toggleClass("active");
+});
+
+//document.querySelector("#hamMenu").addEventListener("click", function () {
+//  this.classList.toggle("active");
+//});
